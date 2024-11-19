093463d85d66574155aad7bc3b03b6b78687d33c
Fr4nKB <fberti.root@gmail.com>
2024-11-19 11:43:28 +0100

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Step 0: Add fingerprints for each VM
+ssh-keyscan -H 10.2.1.4 >> ~/.ssh/known_hosts
+ssh-keyscan -H 10.2.1.5 >> ~/.ssh/known_hosts
+
 # Step 1: Copy folder project to /root/ in both VMs
 sshpass -p 'root' scp -r project root@10.2.1.4:/root/
 sshpass -p 'root' scp -r project root@10.2.1.5:/root/
@@ -7,23 +11,7 @@ sshpass -p 'root' scp -r project root@10.2.1.5:/root/
 # Step 2: Copy pastry.war to the specified directory in 10.2.1.4
 sshpass -p 'root' scp pastry-webserver/pastry.war root@10.2.1.4:/root/servers/apache-tomcat-10.1.16/webapps/
 
-# Step 3: Create three new shells and connect to the VMs using ssh
-gnome-terminal -- bash -c "sshpass -p 'root' ssh root@10.2.1.4; exec bash"
-gnome-terminal -- bash -c "sshpass -p 'root' ssh root@10.2.1.4; exec bash"
-gnome-terminal -- bash -c "sshpass -p 'root' ssh root@10.2.1.5; exec bash"
-
-# Step 4: On both VMs, head into /root/project and execute make, then execute the respective erl commands
-sshpass -p 'root' ssh root@10.2.1.4 << 'EOF'
-cd /root/project
-make
-erl -pa ebin -name node1@10.2.1.4 -setcookie pastry -eval "controller:start(\"control1\", 10, \"node1@10.2.1.4\")."
-EOF
-
-sshpass -p 'root' ssh root@10.2.1.5 << 'EOF'
-cd /root/project
-make
-erl -pa ebin -name node2@10.2.1.5 -setcookie pastry -eval "controller:start(\"control2\", 10, \"node2@10.2.1.5\", {{node1, node1@10.2.1.4}, \"node1\"})."
-EOF
-
-# Step 5: In the second 10.2.1.4 shell, go into /root/servers/apache-tomcat-10.1.16/bin and execute ./startup.sh
+# Step 3: Create three new shells and connect to the VMs using ssh, then execute the commands
+gnome-terminal -- bash -c "sshpass -p 'root' ssh root@10.2.1.4 'cd /root/project && make && erl -pa ebin -name node1@10.2.1.4 -setcookie pastry -eval \"controller:start(\\\"control1\\\", 10, \\\"node1@10.2.1.4\\\").\"'; exec bash"
+gnome-terminal -- bash -c "sshpass -p 'root' ssh root@10.2.1.5 'cd /root/project && make && erl -pa ebin -name node2@10.2.1.5 -setcookie pastry -eval \"controller:start(\\\"control2\\\", 10, \\\"node2@10.2.1.5\\\", {{node1, node1@10.2.1.4}, \\\"node1\\\"}).\"'; exec bash"
 gnome-terminal -- bash -c "sshpass -p 'root' ssh root@10.2.1.4 'cd /root/servers/apache-tomcat-10.1.16/bin && ./startup.sh'; exec bash"
