736e8e7829840832d096ab5f5ac0bf6b0d65cc9e
Iacopo Canetta <80352950+Top-Down@users.noreply.github.com>
2024-11-19 10:14:11 +0100

    file names in controller updated

--- project/src/erlang/controller.erl
+++ project/src/erlang/controller.erl
@@ -1,32 +1,32 @@
 -module(controller).
--export([start/3, start/2]).
+-export([start/3, start/4]).
 -import(node, [start_node/2, start_node/3]).
 
-start(N, NodeName) ->
-    start(N, NodeName, self).
+start(ControllerName, N, NodeName) ->
+    start(ControllerName, N, NodeName, self).
 
-start(N, NodeName, JoinAddr) ->
-    Nodes = spawn_nodes(N, NodeName, [], JoinAddr),
-    loop(NodeName, Nodes, N).
+start(ControllerName, N, NodeName, JoinAddr) ->
+    Nodes = spawn_nodes(ControllerName, N, NodeName, [], JoinAddr),
+    loop(ControllerName, NodeName, Nodes, N).
 
-spawn_nodes(0, NodeName, Nodes, ) -> Nodes;
-spawnnodes(N, NodeName, Nodes, JoinAddr) ->
+spawn_nodes(_, 0, _NodeName, Nodes, _) -> Nodes;
+spawn_nodes(ControllerName, N, NodeName, Nodes, JoinAddr) ->
     NodeAddr = list_to_atom(NodeName),
     NodeId = length(Nodes) + 1,
     Node = "node" ++ integer_to_list(NodeId),
-    create_files(Node),
+    create_files(ControllerName, Node),
     case {JoinAddr, NodeId} of
         {self, 1} -> Pid = start_node(Node, NodeName);
-        {self, } -> Pid = startnode(Node, NodeName, {{node1, NodeAddr}, "node1"});
-        {, _} -> Pid = start_node(Node, NodeName, {{node1, JoinAddr}, "node1"})
+        {self, _} -> Pid = start_node(Node, NodeName, {{node1, NodeAddr}, "node1"});
+        {_ , _} -> Pid = start_node(Node, NodeName, {{node1, JoinAddr}, "node1"})
     end,
-    spawn_nodes(N - 1, NodeName, [{Node, Pid} | Nodes], JoinAddr).
+    spawn_nodes(ControllerName, N - 1, NodeName, [{Node, Pid} | Nodes], JoinAddr).
 
-loop(NodeName, Nodes, LastId) ->
+loop(ControllerName, NodeName, Nodes, LastId) ->
     io:format("Controller started. Enter commands:\n", []),
-    loop(NodeName, Nodes, LastId, []).
+    loop(ControllerName, NodeName, Nodes, LastId, []).
 
-loop(NodeName, Nodes, LastId, Buffer) ->
+loop(ControllerName, NodeName, Nodes, LastId, Buffer) ->
     io:format("> ", []),
     case io:get_line("") of
         eof -> ok;
@@ -38,12 +38,12 @@ loop(NodeName, Nodes, LastId, Buffer) ->
                     Node = "node" ++ integer_to_list(NewLastId),
                     case lists:keyfind(Node, 1, Nodes) of
                         false ->
-                            create_files(Node),
+                            create_files(ControllerName, Node),
                             NodeAddr = list_to_atom(NodeName),
                             Pid = start_node(Node, NodeName, {{node1, NodeAddr}, "node1"}),
-                            loop(NodeName, [{Node, Pid} | Nodes], NewLastId, Buffer);
+                            loop(ControllerName, NodeName, [{Node, Pid} | Nodes], NewLastId, Buffer);
                         _ ->
-                            loop(NodeName, Nodes, LastId, Buffer)
+                            loop(ControllerName, NodeName, Nodes, LastId, Buffer)
                     end;
 
                 ["spawn", Starter] ->
@@ -51,43 +51,44 @@ loop(NodeName, Nodes, LastId, Buffer) ->
                     Node = "node" ++ integer_to_list(NewLastId),
                     case lists:keyfind(Node, 1, Nodes) of
                         false ->
-                            create_files(Node),
+                            create_files(ControllerName, Node),
                             Pid = start_node(Node, NodeName, Starter),
-                            loop(NodeName, [{Node, Pid} | Nodes], NewLastId, Buffer);
+                            loop(ControllerName, NodeName, [{Node, Pid} | Nodes], NewLastId, Buffer);
                         _ ->
-                            loop(NodeName, Nodes, LastId, Buffer)
+                            loop(ControllerName, NodeName, Nodes, LastId, Buffer)
                     end;
 
                 ["spawn", Node, Starter] ->
                     case lists:keyfind(Node, 1, Nodes) of
                         false ->
-                            create_files(Node),
+                            create_files(ControllerName, Node),
                             Pid = start_node(Node, NodeName, Starter),
-                            loop(NodeName, [{Node, Pid} | Nodes], LastId, Buffer);
+                            loop(ControllerName, NodeName, [{Node, Pid} | Nodes], LastId, Buffer);
                         _ ->
-                            loop(NodeName, Nodes, LastId, Buffer)
+                            loop(ControllerName, NodeName, Nodes, LastId, Buffer)
                     end;
 
                 ["kill_node", Node] ->
                     case lists:keyfind(Node, 1, Nodes) of
                         {Node, Pid} ->
                             Pid ! kill_node,
-                            loop(NodeName, lists:keydelete(Node, 1, Nodes), LastId, Buffer);
+                            loop(ControllerName, NodeName, lists:keydelete(Node, 1, Nodes), LastId, Buffer);
                         false ->
-                            loop(NodeName, Nodes, LastId, Buffer)
+                            loop(ControllerName, NodeName, Nodes, LastId, Buffer)
                     end;
 
                 ["off"] ->
                     ok;
 
                 _ ->
-                    loop(NodeName, Nodes, LastId, Buffer)
+                    loop(ControllerName, NodeName, Nodes, LastId, Buffer)
             end
     end.
 
-create_files(Node) ->
-    File1 = "./files/" ++ Node ++ "/" ++ Node ++ "file1.txt",
-    File2 = "./files/" ++ Node ++ "/" ++ Node ++ "file2.txt",
+
+create_files(ControllerName, Node) ->
+    File1 = "./files/" ++ Node ++ "/" ++ ControllerName ++ "_" ++ Node ++ "_file1.txt",
+    File2 = "./files/" ++ Node ++ "/" ++ ControllerName ++ "_" ++ Node ++ "_file2.txt",
     ok = filelib:ensure_dir(File1),
     ok = filelib:ensure_dir(File2),
     Content1 = lists:duplicate(100, Node ++ "file1.txt\n"),
