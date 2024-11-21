099a2896e5fd1fb12fc399a8b87ccaa64a505954
Fr4nKB <fberti.root@gmail.com>
2024-11-21 10:45:52 +0100

    modified deploy and start scripts

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ ssh-keyscan -H 10.2.1.4 >> ~/.ssh/known_hosts
 ssh-keyscan -H 10.2.1.5 >> ~/.ssh/known_hosts
 
 # Step 1: Copy folder project to /root/ in both VMs
-sshpass -p 'root' scp -r project root@10.2.1.4:/root/
-sshpass -p 'root' scp -r project root@10.2.1.5:/root/
+sshpass -p 'root' scp -r project root@10.2.1.4:/root/pastry/
+sshpass -p 'root' scp -r project root@10.2.1.5:/root/pastry/
 
 # Step 2: Copy pastry.war to the specified directory in 10.2.1.4
 sshpass -p 'root' scp pastry-webserver/pastry.war root@10.2.1.4:/root/servers/apache-tomcat-10.1.16/webapps/
